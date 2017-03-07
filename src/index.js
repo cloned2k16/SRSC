00f1791420b216b2997043693be7810453353384
i5 <know@ledg.es>
2017-03-07 17:25:04 +0100

    creating tiles Objects dynamically ..

--- src/index.js
+++ src/index.js
@@ -11,20 +11,7 @@ import                          './index.css';
     ,   _err            =   (... a)     =>  { Function.apply.call(_CON.error   ,_CON,a); }              // eslint-disable-line   
     ,   DB_model        =   {
                 numProducts     :   111
-            ,   productTypes    :  [Banana
-                                ,   Apple
-                                ,   Orange
-                                ,   Chicken
-                                ,   Beef
-                                ,   Aubergine
-                                ,   Broccoli
-                                ,   Zucchini
-                                ,   Baguette
-                                ,   Cherry
-                                ,   Pork
-                                ,   Carrot
-                                ,   Pretzel
-                                ]
+            ,   productTypes    :   []
             ,   rndElement      :   function    (a)             {
                     return a[Math.floor(Math.random()*a.length)];
                 }
@@ -56,6 +43,8 @@ import                          './index.css';
         }
     ;
 //  ----------------------------------- --------------------------- ---------------------------------
+//  ----------------------------------- --------------------------- ---------------------------------
+//  ----------------------------------- --------------------------- ---------------------------------
     function    Extends                 (oo, parent)                {
 
         oo.prototype              = Object.create(parent.prototype);                                    //  just protoype stuff !
@@ -76,113 +65,87 @@ import                          './index.css';
         this.text       =   "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     }
 //  ----------------------------------- --------------------------- ---------------------------------
-    function    Vegetable               ()  {   Vegetable   .prototype.super(this);                     // eslint-disable-line
-        this.color          = '#40F060'
-        this.image          = "url('img/vegetables.jpg')"
-    }
-//  ----------------------------------- --------------------------- ---------------------------------
-    function    Fruit                   ()  {   Fruit       .prototype.super(this);                     // eslint-disable-line
-        this.color          = '#E09040'
-        this.image          = "url('img/fruits.jpg')"
-    }
-//  ----------------------------------- --------------------------- ---------------------------------
-    function    Meat                    ()  {   Meat        .prototype.super(this);                     // eslint-disable-line
-        this.color          = '#E04040'
-        this.image          = "url('img/meats.jpg')"
-    }
-//  ----------------------------------- --------------------------- ---------------------------------
-    function    Bread                   ()  {   Bread       .prototype.super(this);                     // eslint-disable-line
-        this.color          = '#D0D040'
-        this.image          = "url('img/breads.jpg')"
-    }
-//  ----------------------------------- --------------------------- ---------------------------------
-    Extends     (Vegetable  , ShopTile);
-    Extends     (Fruit      , ShopTile);
-    Extends     (Meat       , ShopTile);
-    Extends     (Bread      , ShopTile);
-//  ----------------------------------- --------------------------- ---------------------------------
-    function    Banana                  ()  {   Banana      .prototype.super(this);                     // eslint-disable-line    
-        this.image          = "url('img/banana.jpg')"
-    }
-    function    Apple                   ()  {   Apple       .prototype.super(this); }                   // eslint-disable-line
-    function    Orange                  ()  {   Orange      .prototype.super(this);                     // eslint-disable-line
-        this.image          = "url('img/orange.jpg')"
-    }
-
-    function    Cherry                  ()  {   Cherry      .prototype.super(this);                     // eslint-disable-line
-        this.image          = "url('img/cherry.jpg')"
-        this.backColor='#FFE'; 
-    }
-
-    Extends     (Banana     , Fruit);
-    Extends     (Apple      , Fruit);
-    Extends     (Orange     , Fruit);
-    Extends     (Cherry     , Fruit);
-//  ----------------------------------- --------------------------- ---------------------------------
-    function    Chicken                 ()  {   Chicken     .prototype.super(this);                     // eslint-disable-line
-        this.image          = "url('img/pollo.jpg')"
-    }
-    function    Beef                    ()  {   Beef        .prototype.super(this); }                   // eslint-disable-line
-    function    Pork                    ()  {   Pork        .prototype.super(this);                     // eslint-disable-line
-        this.image          = "url('img/pork.jpg')"
-        this.backColor='#FEE';     
-    }
-
-    Extends     (Chicken    , Meat);
-    Extends     (Beef       , Meat);
-    Extends     (Pork       , Meat);
-//  ----------------------------------- --------------------------- ---------------------------------
-    function    Aubergine               ()  {   Aubergine   .prototype.super(this);                     // eslint-disable-line
-        this.image          = "url('img/aubergine.jpg')"
-    }
-    function    Broccoli                ()  {   Broccoli    .prototype.super(this);                     // eslint-disable-line
-        this.image          = "url('img/broccoli.jpg')"
-    }
-    function    Zucchini                ()  {   Zucchini    .prototype.super(this);                     // eslint-disable-line
-        this.image          = "url('img/zucchini.jpg')"
-    }
-    function    Carrot                  ()  {   Carrot      .prototype.super(this);                     // eslint-disable-line
-        this.image          = "url('img/carrot.jpg')"
-        this.backColor='#FDD'; 
-    }    
-
-    Extends     (Aubergine  , Vegetable);
-    Extends     (Broccoli   , Vegetable);
-    Extends     (Zucchini   , Vegetable);
-    Extends     (Carrot     , Vegetable);
-//  ----------------------------------- --------------------------- ---------------------------------
-    function    Baguette                ()  {   Baguette    .prototype.super(this);                     // eslint-disable-line
-        this.image          = "url('img/baguette.jpg')"
-    }
-    function    Pretzel                 ()  {   Pretzel     .prototype.super(this);                     // eslint-disable-line
-        this.image          = "url('img/pretzel.jpg')"
-        this.backColor='#DFF'; 
-    }                        
-
-    Extends     (Baguette   , Bread);
-    Extends     (Pretzel    , Bread);
-//  ----------------------------------- --------------------------- ---------------------------------
     ShopTile    .prototype.label='NONE'     ;                                                           // eslint-disable-line 
-    Banana      .prototype.label='Banana'   ;                                                           // eslint-disable-line
-    Apple       .prototype.label='Apple'    ;                                                           // eslint-disable-line
-    Orange      .prototype.label='Orange'   ;                                                           // eslint-disable-line
-    Chicken     .prototype.label='Chicken'  ;                                                           // eslint-disable-line
-    Beef        .prototype.label='Beef'     ;                                                           // eslint-disable-line
-    Aubergine   .prototype.label='Aubergine';                                                           // eslint-disable-line
-    Broccoli    .prototype.label='Broccoli' ;                                                           // eslint-disable-line
-    Zucchini    .prototype.label='Zucchini' ;                                                           // eslint-disable-line
-    Baguette    .prototype.label='Baguette' ;                                                           // eslint-disable-line
-    Cherry      .prototype.label='Cherry'   ;                                                           // eslint-disable-line
-    Pork        .prototype.label='Pork'     ;                                                           // eslint-disable-line
-    Carrot      .prototype.label='Carrot'   ;                                                           // eslint-disable-line
-    Pretzel     .prototype.label='Pretzel'  ;                                                           // eslint-disable-line
+//  ----------------------------------- --------------------------- ---------------------------------
+    var products                        =   ['Vegetable'
+                                            ,'Fruit'
+                                            ,'Meat'
+                                            ,'Bread'
+                                        ]
+    ,   vegetables                      =   ['Aubergine'
+                                            ,'Broccoli'
+                                            ,'Zucchini'
+                                            ,'Carrot'
+                                        ]
+    ,   fruits                          =   ['Banana'
+                                            ,'Apple'
+                                            ,'Orange'
+                                            ,'Cherry'
+                                        ]
+    ,   meats                           =   ['Chicken'
+                                            ,'Beef'
+                                            ,'Pork'
+                                        ]
+    ,   breads                          =   ['Baguette'
+                                            ,'Pretzel'
+                                        ]
+    ,   createObjects                   =   (bundle,list)  =>  {
+        var k
+        ,   i
+        ,   o
+        ,   nm
+        ,   oo
+        ,   lst
+        ,   knd
+        ,   bLen            = bundle.length
+        ,   imageExists     = (url) =>  {
+                var http = new XMLHttpRequest();
+                    http.open('HEAD', url, false);
+                    http.send();
+                    return http.status !== 404;
+        }
+        ;
+        
+        for (k=0 ; k< bLen ; k++ ){
+            o   = bundle[k];
+            knd = o.kind;
+            lst = o.list;
+            for (i = 0 ; i< lst.length ; i++ ){
+                nm = lst[i];
+                eval ('window. '+nm+'= function '+nm+'(){ '+nm                                                // eslint-disable-line
+                        +'.prototype.super(this);'
+                        + (imageExists('img/'+nm+'.jpg')
+                        ? 'this.image= "url(img/'+nm+'.jpg)";'
+                        : '')
+                        +'}');
+                oo=eval(nm);                                                                            // eslint-disable-line
+                Extends(oo,knd);            
+                eval (nm+'.prototype.label="'+nm+'"');                                                  // eslint-disable-line
+                eval (nm+'.prototype.image="'+nm+'"');                                                  // eslint-disable-line
+                list.push(oo);
+            }
+        }
+    }
+    ,   categories                      =   [];
+    ;
+    
+    createObjects (    [{ 'kind' : ShopTile  , 'list' : products     }]
+                    ,categories);
+    
+    createObjects (    [{ 'kind' : categories[0] , 'list' : vegetables   }
+                    ,   { 'kind' : categories[1] , 'list' : fruits       }
+                    ,   { 'kind' : categories[2] , 'list' : meats        }
+                    ,   { 'kind' : categories[3] , 'list' : breads       }
+                    ]
+                    ,_APP.model.productTypes);
+    ;
 //  ----------------------------------- --------------------------- ---------------------------------
 //  ----------------------------------- --------------------------- ---------------------------------
 //  ----------------------------------- --------------------------- ---------------------------------
-    var filter                          = [ { 'type' : Meat.name        ,'label' : 'Meats'     , 'val' : true }
-                                        ,   { 'type' : Fruit.name       ,'label' : 'Fruits'    , 'val' : true }
-                                        ,   { 'type' : Vegetable.name   ,'label' : 'Vegetables', 'val' : true }
-                                        ,   { 'type' : Bread.name       ,'label' : 'Breads'    , 'val' : true }
+    var filter                          = [ { 'type' : categories[2].name ,'label' : 'Meats'     , 'val' : true }
+                                        ,   { 'type' : categories[1].name ,'label' : 'Fruits'    , 'val' : true }
+                                        ,   { 'type' : categories[0].name ,'label' : 'Vegetables', 'val' : true }
+                                        ,   { 'type' : categories[3].name ,'label' : 'Breads'    , 'val' : true }
                                         ];
 //  ----------------------------------- --------------------------- ---------------------------------
     var menu                            = ['Home', 'Products', 'Services', 'Contact'];             
